e5368aeba4a8111746c2a7003069a0a140b570b6
Kim Morrison <kim@tqft.net>
2026-08-02 12:42:37 +0000

    refactor: model verified environment entries faithfully

--- Lean4Lean/Theory/VDecl.lean
+++ Lean4Lean/Theory/VDecl.lean
@@ -29,9 +29,6 @@ structure VInductDecl where
   isUnsafe : Bool
 
 inductive VDecl where
-  /-- Reserve a constant name, which cannot be used in expressions.
-  Used to represent unsafe declarations in safe mode -/
-  | block (n : Name)
   | axiom (_ : VConstVal)
   | def (_ : VDefVal)
   | opaque (_ : VDefVal)
